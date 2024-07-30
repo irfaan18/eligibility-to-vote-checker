116fe9602661e3fc42febe42ff2bb0bd02ceaf74
irfaan18 <shaikirfansaleema@gmail.com>
2024-07-30 19:12:16 +0530

    Create Eligibility to vote checker.py

---
+++ Eligibility to vote checker.py
@@ -0,0 +1,8 @@
+# Eligibility to vote checker  
+
+print("Check eligibility to Vote")
+x = int(input("Enter your Age: "))
+if(x >= 18):
+    print("You CAN Vote")
+else:
+    print("You CANNOT Vote")
